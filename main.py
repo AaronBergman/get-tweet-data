9502961bde89e5fa59e85f5d9afc65372678b79f
Aaron Bergman <84301849+AaronBergman@users.noreply.github.com>
2023-07-19 23:08:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,21 @@ def main():
 7. Hit download to access your spreadsheet, which you can load into Excel or Google Sheets
 ''')
 
+    # Embed the PayPal donation form
+    paypal_form = """
+<form action="https://www.paypal.com/donate" method="post" target="_top">
+<input type="hidden" name="business" value="PJBNFHJBWLEVS" />
+<input type="hidden" name="no_recurring" value="0" />
+<input type="hidden" name="item_name" value="Don't give me any money that you'd otherwise donate to an effective charity" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
+
+    """
+
     uploaded_file = st.file_uploader("Upload tweets.js", type=["js"])
+    st.markdown(paypal_form, unsafe_allow_html=True)
 
     if uploaded_file is not None:
         render_file(uploaded_file)
